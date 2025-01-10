aafcff77bac15b8c666ae77b0c7894bf01164fba
Massimiliano Culpo <massimiliano.culpo@gmail.com>
2025-01-10 11:55:21 +0100

    Spec: relax a few type-hints to accept a Sequence

--- lib/spack/spack/spec.py
+++ lib/spack/spack/spec.py
@@ -66,6 +66,7 @@ from typing import (
     List,
     Match,
     Optional,
+    Sequence,
     Set,
     Tuple,
     Union,
@@ -1018,7 +1019,7 @@ class _EdgeMap(collections.abc.Mapping):
         parent: Optional[str] = None,
         child: Optional[str] = None,
         depflag: dt.DepFlag = dt.ALL,
-        virtuals: Optional[List[str]] = None,
+        virtuals: Optional[Sequence[str]] = None,
     ) -> List[DependencySpec]:
         """Selects a list of edges and returns them.
 
@@ -1617,12 +1618,12 @@ class Spec:
         ]
 
     def edges_to_dependencies(
-        self, name=None, depflag: dt.DepFlag = dt.ALL, *, virtuals: Optional[List[str]] = None
+        self, name=None, depflag: dt.DepFlag = dt.ALL, *, virtuals: Optional[Sequence[str]] = None
     ) -> List[DependencySpec]:
         """Returns a list of edges connecting this node in the DAG to children.
 
         Args:
-            name (str): filter dependencies by package name
+            name: filter dependencies by package name
             depflag: allowed dependency types
             virtuals: allowed virtuals
         """
@@ -1655,7 +1656,7 @@ class Spec:
         name=None,
         deptype: Union[dt.DepTypes, dt.DepFlag] = dt.ALL,
         *,
-        virtuals: Optional[List[str]] = None,
+        virtuals: Optional[Sequence[str]] = None,
     ) -> List["Spec"]:
         """Returns a list of direct dependencies (nodes in the DAG)
 
